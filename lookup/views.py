265f620e0ea7136a3f88a725268e0428c4d4c08e
lawrie205 <lawrie205@gmail.com>
2020-03-08 14:53:45 +0530

    view refactored

--- lookup/views.py
+++ lookup/views.py
@@ -16,24 +16,25 @@ def home(request):
         api = json.loads(api_request.content)
     except Exception as e:
         api ="Error"
-        
+
     if api:
-        if api[0]['Category']['Name'] == "Good":
+        aqi_quality = api[0]['Category']['Name']
+        if aqi_quality == "Good":
             category_description = "(0-50) Air quality is considered satisfactory, and air pollution poses little or no risk."
             category_color = "good"
-        elif api[0]['Category']['Name'] == "Moderate":
+        elif aqi_quality == "Moderate":
             category_description = "(51-100) Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."
             category_color = "moderate"
-        elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
+        elif aqi_quality == "Unhealthy for Sensitive Groups":
             category_description = "(101-150) Although general public is not likely to be affected at this AQI range, people with lung disease, older adults and children are at a greater risk from exposure to ozone, whereas persons with heart and lung disease, older adults and children are at greater risk from the presence of particles in the air."
             category_color = "usg"
-        elif api[0]['Category']['Name'] == "Unhealthy":
+        elif aqi_quality == "Unhealthy":
             category_description = "(151-200) Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects."
             category_color = "unhealthy"
-        elif api[0]['Category']['Name'] == "Very Unhealthy":
+        elif aqi_quality == "Very Unhealthy":
             category_description = "(201-300) Health alert: everyone may experience more serious health effects."
-            category_color = "verunhealthy"
-        elif api[0]['Category']['Name'] == "Hazardous":
+            category_color = "veryunhealthy"
+        elif aqi_quality == "Hazardous":
             category_description = "(301-500) Health warnings of emergency conditions. The entire population is more likely to be affected."
             category_color = "hazardous"
     else:
